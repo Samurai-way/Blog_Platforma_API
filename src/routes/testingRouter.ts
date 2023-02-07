6c3a9dccd22ea52c38be32f804f5f43261ca0183
Samurai-way <olegmoishevych@gmail.com>
2023-02-07 12:12:43 +0200

    feat: dall, delete request

--- src/routes/testingRouter.ts
+++ src/routes/testingRouter.ts
@@ -1,4 +1,9 @@
-import {Router} from "express";
+import {Request, Response, Router} from "express";
 
+let blogs = []
 
 export const testingRouter = Router({})
+testingRouter.delete('/', (req: Request, res: Response) => {
+    blogs = []
+    res.send(204)
+})
