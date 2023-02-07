f7e57303af7792a60dad3b709189fde83c088a32
Samurai-way <olegmoishevych@gmail.com>
2023-02-07 17:22:13 +0200

    fix: bugs

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -12,12 +12,12 @@ export const websiteUrl = body('websiteUrl').trim().isLength({
     min: 1,
     max: 100
 }).matches("^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$").withMessage('description maxLength: 500')
-export const title = body('title').trim().isLength({min: 1, max: 30}).withMessage('title maxLength: 30')
+export const title = body('title').trim().isLength({min: 1, max: 30}).withMessage('title maxLength: 30').isString()
 export const shortDescription = body('shortDescription').trim().isLength({
     min: 1,
     max: 100
-}).withMessage('shortDescription maxLength: 30')
-export const content = body('content').trim().isLength({min: 1, max: 1000}).withMessage('content maxLength: 1000')
+}).withMessage('shortDescription maxLength: 100').isString()
+export const content = body('content').trim().isLength({min: 1, max: 1000}).withMessage('content maxLength: 1000').isString()
 export const blogId = body('blogId').isString().isLength({min: 1}).withMessage('blogId')
 
 export const postBlogValidator = [name, description, websiteUrl, basicAuthMiddleware,inputValidationMiddleware]
