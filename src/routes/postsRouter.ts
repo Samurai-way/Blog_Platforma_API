62d5e219fabb34e351d31aafc17fdc5590f19296
Samurai-way <olegmoishevych@gmail.com>
2023-02-08 21:22:33 +0200

    fix: bugs

--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -6,38 +6,38 @@ import {basicAuthMiddleware} from "../middlewares/basicAuthMiddleware";
 
 export const postsRouter = Router({})
 
-postsRouter.get('/', (req: Request, res: Response) => {
-    const findPosts = postsRepository.getPosts()
+postsRouter.get('/', async (req: Request, res: Response) => {
+    const findPosts = await postsRepository.getPosts()
     res.status(200).send(findPosts)
 })
-postsRouter.post('/', postPostsValidator, (req: Request, res: Response) => {
+postsRouter.post('/', postPostsValidator, async (req: Request, res: Response) => {
     const {title, shortDescription, content, blogId} = req.body
-    const createPost = postsRepository.createPost(title, shortDescription, content, blogId)
+    const createPost = await postsRepository.createPost(title, shortDescription, content, blogId)
     res.status(201).send(createPost)
 })
-postsRouter.get('/:id', (req: Request, res: Response) => {
+postsRouter.get('/:id', async (req: Request, res: Response) => {
     const id = req.params.id
-    const findPost = postsRepository.getPostById(id)
+    const findPost = await postsRepository.getPostById(id)
     if (findPost) {
         res.status(200).send(findPost)
     } else {
         res.send(404)
     }
 })
-postsRouter.put('/:id', postPostsValidator, (req: Request, res: Response) => {
+postsRouter.put('/:id', postPostsValidator, async (req: Request, res: Response) => {
     const id = req.params.id
     const {title, shortDescription, content, blogId} = req.body
-    const updatePost = postsRepository.updatePostById(id, title, shortDescription, content, blogId)
+    const updatePost = await postsRepository.updatePostById(id, title, shortDescription, content, blogId)
     if (updatePost) {
-        const findPost = postsRepository.getPostById(id)
+        const findPost = await postsRepository.getPostById(id)
         res.status(204).send(findPost)
     } else {
         res.send(404)
     }
 })
-postsRouter.delete('/:id', basicAuthMiddleware, (req: Request, res: Response) => {
+postsRouter.delete('/:id', basicAuthMiddleware, async (req: Request, res: Response) => {
     const id = req.params.id
-    const deletePost = postsRepository.deletePostById(id)
+    const deletePost = await postsRepository.deletePostById(id)
     if (deletePost) {
         res.send(204)
     } else {
