f39f5c459607ede71d1e58de4cd694da92c504f6
Samurai-way <olegmoishevych@gmail.com>
2023-02-13 18:06:49 +0200

    fix: bugs

--- src/repositories/testing-db-repository.ts
+++ src/repositories/testing-db-repository.ts
@@ -1,9 +1,10 @@
-import {blogsCollection, postsCollection} from "../db/db";
+import {blogsCollection, postsCollection, usersCollection} from "../db/db";
 
 export const testingDbRepository = {
     async deleteAllData(): Promise<boolean> {
         const clearBlogs = await blogsCollection.deleteMany({})
         const clearPosts = await postsCollection.deleteMany({})
-        return clearBlogs.deletedCount === 1 && clearPosts.deletedCount === 1
+        const clearUsers = await usersCollection.deleteMany({})
+        return clearBlogs.deletedCount === 1 && clearPosts.deletedCount === 1 && clearUsers.deletedCount === 1
     }
 }
