8ba558d6d68953d468dff2dc25fd73ecdd95e311
Samurai-way <olegmoishevych@gmail.com>
2023-02-13 12:18:18 +0200

    feat: auth router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import {Router} from "express";
 import {blogsRouter} from "./blogsRouter";
 import {postsRouter} from "./postsRouter";
 import {testingRouter} from "./testingRouter";
+import {authRouter} from "./authRouter";
 
 export const routes = Router({})
 
+routes.use('/auth', authRouter)
 routes.use('/blogs', blogsRouter)
 routes.use('/posts', postsRouter)
 routes.use('/testing/all-data', testingRouter)
