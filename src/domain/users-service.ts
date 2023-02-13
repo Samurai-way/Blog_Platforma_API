f59006488013ef3d07e8c94d94a51f2448c3dc5d
Samurai-way <olegmoishevych@gmail.com>
2023-02-13 12:57:08 +0200

    feat: create newUser

--- src/domain/users-service.ts
+++ src/domain/users-service.ts
@@ -1,17 +1,31 @@
 import {ObjectId} from "mongodb";
+import bcrypt from "bcrypt";
 
 export type DB_User_Type = {
     _id: ObjectId,
-    id: string,
-    login: string,
-    password: string,
-    email: string
+    userName: string,
+    passwordHash: string
+    passwordSalt: string
+    email: string,
+    createAt: Date
 
 }
 export const usersService = {
-    async createUser(login:string, password:string, email:string){
-        const user: DB_User_Type = {
-
+    async createUser(login: string, password: string, email: string) {
+        const passwordSalt = await bcrypt.genSalt(10)
+        const passwordHash = await this._generationHash(password, passwordSalt)
+        const newUser: DB_User_Type = {
+            _id: new ObjectId(),
+            userName: login,
+            email,
+            passwordHash,
+            passwordSalt,
+            createAt: new Date()
         }
+    },
+    async _generationHash(password: string, salt: string) {
+        const hash = await bcrypt.hash(password, salt)
+        console.log('hash', +hash)
+        return hash
     }
 }
