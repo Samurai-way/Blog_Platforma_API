a5b453f45d0cd25f96796cc8b530eb9b456b018b
Samurai-way <olegmoishevych@gmail.com>
2023-02-14 12:00:39 +0200

    feat: jsonwebtoken

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -7,7 +7,6 @@ export const authRouter = Router({})
 authRouter.post('/login', async (req: Request, res: Response) => {
     const {loginOrEmail, password} = req.body
     const checkResult = await usersService.checkCredentials(loginOrEmail, password)
-    // console.log(checkResult)
     if (!checkResult) return res.sendStatus(401)
-    res.status(204).send(checkResult)
+    // const token = await
 })
