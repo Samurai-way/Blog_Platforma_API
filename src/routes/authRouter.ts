a3435812598ab9eba4e4d3a705fb1c740b2c15f4
Samurai-way <olegmoishevych@gmail.com>
2023-02-16 15:05:34 +0200

    fix: bugs

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -37,9 +37,9 @@ authRouter.post('/registration', login, password, email, ExpressErrorValidator, 
     const findByEmail = await usersService.findUserByEmail(email)
 
     if (findByLogin?.login === login) return res.status(400).send({
-        errorsMessages: [{message: "Invalid login", field: "login"}]})
+        "errorsMessages": [{"message": "Invalid login", "field": "login"}]})
     if (findByEmail?.email === email) return res.status(400).send({
-        errorsMessages: [{message: "Invalid login", field: "login"}]})
+        "errorsMessages": [{"message": "Invalid login", "field": "login"}]})
     const user = await usersService.createUser(login, password, email)
     if (!user) return res.sendStatus(404)
     res.send(204)
