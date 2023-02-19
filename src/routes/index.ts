7d97307e738ca42cf65492cbb680441a9b6617a7
Samurai-way <olegmoishevych@gmail.com>
2023-02-19 14:39:29 +0200

    feat: add securityDevicesRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,12 @@ import {testingRouter} from "./testingRouter";
 import {authRouter} from "./authRouter";
 import {usersRouter} from "./usersRouter";
 import {commentsRouter} from "./commentsRouter";
+import {securityDevicesRouter} from "./securityDevicesRouter";
 
 export const routes = Router({})
 
 routes.use('/auth', authRouter)
+routes.use('/security', securityDevicesRouter)
 routes.use('/users', usersRouter)
 routes.use('/blogs', blogsRouter)
 routes.use('/posts', postsRouter)
