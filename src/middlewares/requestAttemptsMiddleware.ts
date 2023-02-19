033454cc203912026dd3f412e78f4d1550a515fd
Samurai-way <olegmoishevych@gmail.com>
2023-02-19 15:49:55 +0200

    fix: bugs

--- src/middlewares/requestAttemptsMiddleware.ts
+++ src/middlewares/requestAttemptsMiddleware.ts
@@ -4,6 +4,6 @@ import {attemptsRepository} from "../repositories/attempts-db-repository";
 export const requestAttemptsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const timeLimit = new Date(new Date().getTime() - 1000) // date time
     const countOfAttempts = await attemptsRepository.countOfAttempts(req.ip, req.url, timeLimit)
-    await attemptsRepository.addAttempts(req.ip, req.url, new Date())
+    await attemptsRepository.addAttempts(req.ip, req.url, new Date().toISOString())
     countOfAttempts < 5 ? next() : res.sendStatus(429)
 }
--- src/repositories/attempts-db-repository.ts
+++ src/repositories/attempts-db-repository.ts
@@ -1,11 +1,11 @@
 import {attemptsCollection} from "../db/db";
 
 
-export const attemptsRepository  = {
-    async addAttempts(userIP: string, url: string, time: Date){
-      return  attemptsCollection.insertMany({userIP, url, time})
+export const attemptsRepository = {
+    async addAttempts(userIP: string, url: string, time: string) {
+        return attemptsCollection.insertOne({userIP, url, time})
     },
-    async countOfAttempts(userIP: string, url: string, timeLimit: Date){
+    async countOfAttempts(userIP: string, url: string, timeLimit: Date) {
         return attemptsCollection.countDocuments({userIP, url, time: {$gt: timeLimit}})
     }
 }
