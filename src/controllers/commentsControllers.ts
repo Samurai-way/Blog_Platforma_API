c4d500db05d87e9beb2519d4aa6961438e444b29
Samurai-way <olegmoishevych@gmail.com>
2023-02-22 23:44:36 +0200

    feat: CommentsControllers

---
+++ src/controllers/commentsControllers.ts
@@ -0,0 +1,33 @@
+import {Request, Response} from "express";
+import {commentsService} from "../domain/comments-service";
+import {commentsRepository} from "../repositories/comments-db-repository";
+
+class CommentsControllers {
+    async getCommentsById(req: Request, res: Response){
+        const commentId = req.params.commentId
+        const getCommentById = await commentsService.getCommentById(commentId)
+        if (!getCommentById) return res.send(404)
+        res.status(200).send(getCommentById)
+    }
+    async deleteCommentById(req: Request, res: Response){
+        const commentID = req.params.commentId
+        const user = req.user!
+        const getCommentByID = await commentsRepository.getCommentById(commentID)
+        if (!getCommentByID) return res.send(404)
+        const deletedComment = await commentsService.deleteCommentByID(commentID, user)
+        if (!deletedComment) return res.send(403)
+        res.send(204)
+    }
+    async updateCommentById(req: Request, res: Response){
+        const commentID = req.params.commentId
+        const user = req.user!
+        const content = req.body.content
+        const getCommentById = await commentsService.getCommentById(commentID)
+        if (!getCommentById) return res.sendStatus(404)
+        const updatedComment = await commentsService.updateCommentById(commentID, content, user)
+        if (!updatedComment) return res.sendStatus(403)
+        res.send(204)
+    }
+}
+
+export const commentsControllers = new CommentsControllers()
--- src/routes/commentsRouter.ts
+++ src/routes/commentsRouter.ts
@@ -1,36 +1,11 @@
-import {Request, Response, Router} from "express";
-import {commentsService} from "../domain/comments-service";
+import {Router} from "express";
 import {authMiddleware} from "../middlewares/authMiddleware";
-import {commentsRepository} from "../repositories/comments-db-repository";
 import {ExpressErrorValidator} from "../middlewares/expressErrorValidator";
 import {contentForComments} from "../validators/validators";
+import {commentsControllers} from "../controllers/commentsControllers";
 
 export const commentsRouter = Router({})
 
-commentsRouter.get('/:commentId', async (req: Request, res: Response) => {
-    const commentId = req.params.commentId
-    const getCommentById = await commentsService.getCommentById(commentId)
-    if (!getCommentById) return res.send(404)
-    res.status(200).send(getCommentById)
-})
-
-commentsRouter.delete('/:commentId', authMiddleware, async (req: Request, res: Response) => {
-    const commentID = req.params.commentId
-    const user = req.user!
-    const getCommentByID = await commentsRepository.getCommentById(commentID)
-    if (!getCommentByID) return res.send(404)
-    const deletedComment = await commentsService.deleteCommentByID(commentID, user)
-    if (!deletedComment) return res.send(403)
-    res.send(204)
-})
-
-commentsRouter.put('/:commentId', authMiddleware, contentForComments, ExpressErrorValidator, async (req: Request, res: Response) => {
-    const commentID = req.params.commentId
-    const user = req.user!
-    const content = req.body.content
-    const getCommentById = await commentsService.getCommentById(commentID)
-    if (!getCommentById) return res.sendStatus(404)
-    const updatedComment = await commentsService.updateCommentById(commentID, content, user)
-    if (!updatedComment) return res.sendStatus(403)
-    res.send(204)
-})
+commentsRouter.get('/:commentId', commentsControllers.getCommentsById)
+commentsRouter.delete('/:commentId', authMiddleware, commentsControllers.deleteCommentById)
+commentsRouter.put('/:commentId', authMiddleware, contentForComments, ExpressErrorValidator, commentsControllers.updateCommentById)
