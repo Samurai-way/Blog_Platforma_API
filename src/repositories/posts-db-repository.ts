b8965b7b574795f0ee06a2c958d36e36507058ef
Samurai-way <olegmoishevych@gmail.com>
2023-02-22 23:12:52 +0200

    feat: update PostsRepository

--- src/repositories/posts-db-repository.ts
+++ src/repositories/posts-db-repository.ts
@@ -2,8 +2,7 @@ import {paginator} from "../helpers/pagination";
 import {CommentsModel, PostsModel} from "../db/db";
 import {CommentDBModalType, CommentsType, DB_PostsType, PostsType} from "../types";
 
-
-export const postsRepository = {
+class PostsRepository {
     async getPosts(pageNumber: number, pageSize: number, sortBy: any, sortDirection: any) {
         const findAndSortedPosts = await PostsModel
             .find({}, {_id: 0, __v: 0})
@@ -13,12 +12,12 @@ export const postsRepository = {
             .lean()
         const getCountPosts = await PostsModel.countDocuments({})
         return paginator(pageNumber, pageSize, getCountPosts, findAndSortedPosts)
-    },
+    }
     async createPostComment(newComment: CommentDBModalType): Promise<CommentsType> {
         const result = await CommentsModel.insertMany(newComment)
         const {_id, postId, ...comment} = newComment
         return comment
-    },
+    }
     async findBlogPostByBlogID(pageNumber: number, pageSize: number, sortBy: any, sortDirection: any, blogId: string) {
         const findBlog = await PostsModel
             .find({blogId}, {_id: 0, __v: 0})
@@ -28,20 +27,20 @@ export const postsRepository = {
             .lean()
         const getCountPosts = await PostsModel.countDocuments({blogId})
         return paginator(pageNumber, pageSize, getCountPosts, findBlog)
-    },
+    }
     async createPost(newPost: PostsType): Promise<DB_PostsType | null> {
         const result = await PostsModel.insertMany(newPost)
         const {_id, ...postsCopy} = newPost
         return postsCopy
-    },
+    }
     async createNewBlogPost(newBlogPost: PostsType): Promise<DB_PostsType | null> {
         const result = await PostsModel.insertMany(newBlogPost)
         const {_id, ...newBlogCopy} = newBlogPost
         return newBlogCopy
-    },
+    }
     async getPostById(id: string): Promise<PostsType | null> {
         return PostsModel.findOne({id}, {_id: 0, __v: 0})
-    },
+    }
     async updatePostById(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean> {
         const result = await PostsModel.updateOne({id}, {
             $set: {
@@ -49,9 +48,10 @@ export const postsRepository = {
             }
         })
         return result.matchedCount === 1
-    },
+    }
     async deletePostById(id: string): Promise<boolean> {
         const result = await PostsModel.deleteOne({id})
         return result.deletedCount === 1
     }
 }
+export const postsRepository = new PostsRepository()
