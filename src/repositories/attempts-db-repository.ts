e44f1c6ba7de793458449f8056e0dde86cdef0f6
Samurai-way <olegmoishevych@gmail.com>
2023-02-22 23:09:24 +0200

    feat: update AttemptsRepository

--- src/repositories/attempts-db-repository.ts
+++ src/repositories/attempts-db-repository.ts
@@ -1,10 +1,12 @@
 import {AttemptsModel} from "../db/db";
 
-export const attemptsRepository = {
+class AttemptsRepository {
     async addAttempts(userIP: string, url: string, time: Date) {
         return AttemptsModel.insertMany({userIP, url, time})
-    },
+    }
     async countOfAttempts(userIP: string, url: string, timeLimit: Date) {
         return AttemptsModel.countDocuments({userIP, url, time: {$gt: timeLimit}})
     }
 }
+
+export const attemptsRepository = new AttemptsRepository()
