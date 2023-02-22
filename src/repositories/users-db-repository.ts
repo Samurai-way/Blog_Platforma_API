639d320966034a1744827bc6ff63a508d3e6b300
Samurai-way <olegmoishevych@gmail.com>
2023-02-22 23:14:40 +0200

    feat: update UsersRepository

--- src/repositories/users-db-repository.ts
+++ src/repositories/users-db-repository.ts
@@ -2,7 +2,7 @@ import {paginator} from "../helpers/pagination";
 import {RecoveryCodeModel, UsersModel} from "../db/db";
 import {DB_User_Type, RecoveryCodeType, UserType} from "../types";
 
-export const usersRepository = {
+class UsersRepository {
     async getUser(sortBy: any, sortDirection: any, pageNumber: number, pageSize: number, searchLoginTerm: any, searchEmailTerm: any) {
         const findAndSortedUser = await UsersModel
             .find({
@@ -27,24 +27,24 @@ export const usersRepository = {
             }, {email: {$regex: searchEmailTerm, $options: "i"}}]
         })
         return paginator(pageNumber, pageSize, getCountUsers, findAndSortedUser)
-    },
+    }
     async findUserByLoginOrEmail(loginOrEmail: string) {
         return UsersModel.findOne({$or: [{email: loginOrEmail}, {login: loginOrEmail}]})
-    },
+    }
     async findUserByEmail(email: string) {
         return UsersModel.findOne({email})
-    },
+    }
     async findUserByID(id: string): Promise<DB_User_Type | null> {
         return UsersModel.findOne({id})
-    },
+    }
     async findUserByCode(code: string) {
         return UsersModel.findOne({'emailConfirmation.confirmationCode': code})
-    },
+    }
     async createUser(newUser: DB_User_Type | any): Promise<UserType> {
         const result = await UsersModel.insertMany(newUser)
         const {_id, passwordHash, emailConfirmation, ...newUserCopy} = newUser
         return newUserCopy
-    },
+    }
     async updateUserConfirmationDate(user: DB_User_Type) {
         const updateUser = await UsersModel.updateOne({id: user.id}, {
             $set:
@@ -55,22 +55,23 @@ export const usersRepository = {
                 }
         })
         return updateUser.matchedCount === 1
-    },
+    }
     async updateUserConfirmation(id: string) {
         const updateUser = await UsersModel.updateOne({id}, {$set: {'emailConfirmation.isConfirmed': true}})
         return updateUser.matchedCount === 1
-    },
+    }
     async deleteUser(id: string): Promise<boolean> {
         const result = await UsersModel.deleteOne({id})
         return result.deletedCount === 1
-    },
+    }
     async addRecoveryUserCode(recoveryCode: RecoveryCodeType) {
         return RecoveryCodeModel.insertMany(recoveryCode)
-    },
+    }
     async findUserByRecoveryCode(recoveryCode: string) {
         return RecoveryCodeModel.findOne({recoveryCode})
-    },
+    }
     async updateUserHash(email: string, passwordHash: string){
         return UsersModel.updateOne({email}, {$set: {passwordHash: passwordHash}})
     }
 }
+export const usersRepository = new UsersRepository()
