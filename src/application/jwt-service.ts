feaedc5e46c3b06008b37126e31e2d404160f6c8
Samurai-way <olegmoishevych@gmail.com>
2023-02-23 00:01:54 +0200

    feat: JwtService

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -3,15 +3,17 @@ import {settings} from "./settings";
 import {jwtRepository} from "../repositories/jwt-db-repository";
 import {DB_User_Type} from "../types";
 
-export const jwtService = {
+class JwtService {
     createJWT(user: DB_User_Type, deviceId: string) {
         const accessToken = jwt.sign({userID: user.id, deviceId: deviceId}, settings.JWT_SECRET, {expiresIn: '10s'})
         const refreshToken = jwt.sign({userID: user.id, deviceId: deviceId}, settings.JWT_SECRET, {expiresIn: '20s'})
         return {accessToken, refreshToken}
-    },
+    }
+
     async addRefreshTokenInBlackList(refreshToken: string) {
         return jwtRepository.addRefreshTokenInBlackList(refreshToken)
-    },
+    }
+
     async getJwtPayloadFromRefreshToken(token: string) {
         try {
             const res: any = jwt.verify(token, settings.JWT_SECRET)
@@ -19,9 +21,12 @@ export const jwtService = {
         } catch (error) {
             return null
         }
-    },
+    }
+
     getLastActiveDateFromToken(refreshToken: string): string {
         const payload: any = jwt.decode(refreshToken)
         return new Date(payload.iat * 1000).toISOString()
     }
 }
+
+export const jwtService = new JwtService()
