5efed7f035972e784c51a0297cd55c21d8682548
Samurai-way <olegmoishevych@gmail.com>
2023-02-22 23:15:23 +0200

    feat: update UsersSessionRepository

--- src/repositories/usersSession-db-repository.ts
+++ src/repositories/usersSession-db-repository.ts
@@ -1,31 +1,30 @@
 import {UserSessionsType} from "../types";
 import {UsersSessionModel} from "../db/db";
 
-
-export const usersSessionRepository = {
+class UsersSessionRepository {
     async createNewUserSession(userSession: UserSessionsType) {
         await UsersSessionModel.insertMany(userSession)
-    },
+    }
     async getSessionByUserID(userId: string) {
         return UsersSessionModel.find({userId}, {_id: 0, userId: 0, __v: 0}).lean()
-    },
+    }
     async deleteAllDevice(userId: string, deviceId: string): Promise<boolean> {
         const deleteAllSession = await UsersSessionModel.deleteMany({userId, deviceId: {$ne: deviceId}})
         return deleteAllSession.deletedCount === 1
-    },
+    }
     async deleteDeviceByDeviceID(userId: string, deviceId: string) {
         const result = await UsersSessionModel.deleteOne({userId, deviceId: deviceId})
         return result.deletedCount === 1
-    },
+    }
     async findDeviceByDeviceId(deviceId: string) {
         return UsersSessionModel.findOne({deviceId})
-    },
+    }
     async findOneByDeviceIdUserIdAndLastActiveDate(userId: string, deviceId: string, lastActiveDate: string) {
         return UsersSessionModel.findOne({userId, deviceId, lastActiveDate})
-    },
+    }
     async findDeviceByUserId(userId: string) {
         return UsersSessionModel.findOne({userId})
-    },
+    }
     async updateUserSession(newSession: UserSessionsType) {
         return UsersSessionModel.updateOne({
             userId: newSession.userId,
@@ -33,3 +32,4 @@ export const usersSessionRepository = {
         }, {$set: newSession})
     }
 }
+export const usersSessionRepository = new UsersSessionRepository()
