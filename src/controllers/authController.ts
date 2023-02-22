1c1eed4acb40174f6c6d10a15215d6ea99242ab8
Samurai-way <olegmoishevych@gmail.com>
2023-02-22 23:29:35 +0200

    feat: authController

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,11 @@
 import {Request, Response} from "express";
 import {usersRepository} from "../repositories/users-db-repository";
 import {authService} from "../domain/auth-service";
+import {usersService} from "../domain/users-service";
+import {queryRepository} from "../queryRepository/queryRepository";
+import {jwtService} from "../application/jwt-service";
+import {userSessionService} from "../domain/userSession-service";
+import {email} from "../validators/validators";
 
 class AuthController {
     async getUser(req: Request, res: Response) {
@@ -34,6 +39,68 @@ class AuthController {
         res.status(200).send({accessToken: newTokenPair.accessToken})
     }
 
+    async registration(req: Request, res: Response) {
+        const {login, password, email} = req.body
+        const findByLogin = await usersService.findUserByLogin(login)
+        const findByEmail = await usersService.findUserByEmail(email)
+        if (findByLogin?.login === login) return res.status(400).send({
+            errorsMessages: [{message: login, field: "login"}]
+        })
+        if (findByEmail?.email === email) return res.status(400).send({
+            errorsMessages: [{message: email, field: "email"}]
+        })
+        const user = await usersService.createUser(login, password, email)
+        if (!user) return res.sendStatus(404)
+        res.send(204)
+    }
+
+    async registrationConfirmation(req: Request, res: Response) {
+        const code = req.body.code
+        const error = {errorsMessages: [{message: code, field: "code"}]}
+        const findUserByCode = await usersService.findUserByCode(code)
+        if (!findUserByCode) return res.status(400).send(error)
+        if (findUserByCode.emailConfirmation.isConfirmed) return res.status(400).send(error)
+        await usersService.confirmEmail(code, findUserByCode)
+        res.send(204)
+    }
+
+    async registrationEmailResending(req: Request, res: Response) {
+        const email = req.body.email
+        const findUserByEmail = await usersService.findUserByEmail(email)
+        if (!findUserByEmail || findUserByEmail.emailConfirmation.isConfirmed) return res.status(400).send({
+            errorsMessages: [{
+                message: email,
+                field: "email"
+            }]
+        })
+        await queryRepository.resendingEmail(email, findUserByEmail)
+        return res.sendStatus(204)
+    }
+
+    async logout(req: Request, res: Response) {
+        const refreshToken = req.cookies.refreshToken!
+        const {userID, deviceId} = await jwtService.getJwtPayloadFromRefreshToken(refreshToken)
+        const clearTokensPair = await jwtService.addRefreshTokenInBlackList(refreshToken)
+        if (!clearTokensPair) return res.sendStatus(401)
+        await userSessionService.deleteDeviceByDeviceID(userID, deviceId)
+        res.sendStatus(204)
+    }
+    async passwordRecovery(req: Request, res: Response) {
+        const email = req.body.email
+        await usersService.findUserByEmailAndSendHimLetter(email)
+        res.sendStatus(204)
+    }
+    async newPassword(req: Request, res: Response){
+        const {newPassword, recoveryCode} = req.body
+        const findUserRecoveryCodeAndChangeNewPassword = await usersService.findUserRecoveryCodeAndChangeNewPassword(newPassword, recoveryCode)
+        if (!findUserRecoveryCodeAndChangeNewPassword) return res.status(400).send({
+            errorsMessages: [{
+                message: "Error",
+                field: "recoveryCode"
+            }]
+        })
+        res.sendStatus(204)
+    }
 }
 
 export const authController = new AuthController()
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,14 +1,9 @@
-import {Request, Response, Router} from "express";
-import {usersService} from "../domain/users-service";
-import {jwtService} from "../application/jwt-service";
+import {Router} from "express";
 import {authMiddleware} from "../middlewares/authMiddleware";
 import {email, login, newPassword, password} from "../validators/validators";
 import {ExpressErrorValidator} from "../middlewares/expressErrorValidator";
-import {queryRepository} from "../queryRepository/queryRepository";
 import {refreshTokenMiddleware} from "../middlewares/refreshTokenMiddleware";
 import {requestAttemptsMiddleware} from "../middlewares/requestAttemptsMiddleware";
-import {authService} from "../domain/auth-service";
-import {userSessionService} from "../domain/userSession-service";
 import {authController} from "../controllers/authController";
 
 
@@ -16,70 +11,10 @@ export const authRouter = Router({})
 
 authRouter.get('/me', authMiddleware, authController.getUser)
 authRouter.post('/login', requestAttemptsMiddleware, authController.loginUser)
-
 authRouter.post('/refresh-token', refreshTokenMiddleware, authController.refreshToken)
-
-
-authRouter.post('/registration', login, password, email, requestAttemptsMiddleware, ExpressErrorValidator, async (req: Request, res: Response) => {
-    const {login, password, email} = req.body
-    const findByLogin = await usersService.findUserByLogin(login)
-    const findByEmail = await usersService.findUserByEmail(email)
-    if (findByLogin?.login === login) return res.status(400).send({
-        errorsMessages: [{message: login, field: "login"}]
-    })
-    if (findByEmail?.email === email) return res.status(400).send({
-        errorsMessages: [{message: email, field: "email"}]
-    })
-    const user = await usersService.createUser(login, password, email)
-    if (!user) return res.sendStatus(404)
-    res.send(204)
-})
-
-authRouter.post('/registration-confirmation', requestAttemptsMiddleware, async (req: Request, res: Response) => {
-    const code = req.body.code
-    const error = {errorsMessages: [{message: code, field: "code"}]}
-    const findUserByCode = await usersService.findUserByCode(code)
-    if (!findUserByCode) return res.status(400).send(error)
-    if (findUserByCode.emailConfirmation.isConfirmed) return res.status(400).send(error)
-    await usersService.confirmEmail(code, findUserByCode)
-    res.send(204)
-})
-authRouter.post('/registration-email-resending', requestAttemptsMiddleware, email, ExpressErrorValidator, async (req: Request, res: Response) => {
-    const email = req.body.email
-    const findUserByEmail = await usersService.findUserByEmail(email)
-    if (!findUserByEmail || findUserByEmail.emailConfirmation.isConfirmed) return res.status(400).send({
-        errorsMessages: [{
-            message: email,
-            field: "email"
-        }]
-    })
-    await queryRepository.resendingEmail(email, findUserByEmail)
-    return res.sendStatus(204)
-})
-
-authRouter.post('/logout', refreshTokenMiddleware, async (req: Request, res: Response) => {
-    const refreshToken = req.cookies.refreshToken!
-    const {userID, deviceId} = await jwtService.getJwtPayloadFromRefreshToken(refreshToken)
-    const clearTokensPair = await jwtService.addRefreshTokenInBlackList(refreshToken)
-    if (!clearTokensPair) return res.sendStatus(401)
-    await userSessionService.deleteDeviceByDeviceID(userID, deviceId)
-    res.sendStatus(204)
-})
-
-authRouter.post('/password-recovery', requestAttemptsMiddleware, email, ExpressErrorValidator, async (req: Request, res: Response) => {
-    const email = req.body.email
-    await usersService.findUserByEmailAndSendHimLetter(email)
-    res.sendStatus(204)
-})
-
-authRouter.post('/new-password', requestAttemptsMiddleware, newPassword, ExpressErrorValidator, async (req: Request, res: Response) => {
-    const {newPassword, recoveryCode} = req.body
-    const findUserRecoveryCodeAndChangeNewPassword = await usersService.findUserRecoveryCodeAndChangeNewPassword(newPassword, recoveryCode)
-    if (!findUserRecoveryCodeAndChangeNewPassword) return res.status(400).send({
-        errorsMessages: [{
-            message: "Error",
-            field: "recoveryCode"
-        }]
-    })
-    res.sendStatus(204)
-})
+authRouter.post('/registration', login, password, email, requestAttemptsMiddleware, ExpressErrorValidator, authController.registration)
+authRouter.post('/registration-confirmation', requestAttemptsMiddleware, authController.registrationConfirmation)
+authRouter.post('/registration-email-resending', requestAttemptsMiddleware, email, ExpressErrorValidator, authController.registrationEmailResending)
+authRouter.post('/logout', refreshTokenMiddleware, authController.logout)
+authRouter.post('/password-recovery', requestAttemptsMiddleware, email, ExpressErrorValidator, authController.passwordRecovery)
+authRouter.post('/new-password', requestAttemptsMiddleware, newPassword, ExpressErrorValidator, authController.newPassword)
